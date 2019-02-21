c7a3dbc84d04e6ee841af14d3eea94f153b6d4f5
Zhu, Nan <nz18977@imcap.ap.ssmb.com>
2019-02-21 17:38:34 +0800

    add listener test

---
+++ src/com/listener/Even.java
@@ -0,0 +1,24 @@
+package com.listener;
+
+public class Even {
+	private Person person;
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+	public Even(Person person) {
+		super();
+		this.person = person;
+	}
+
+	public Even() {
+		super();
+	}
+	
+	
+}
---
+++ src/com/listener/PersonListener.java
@@ -0,0 +1,7 @@
+package com.listener;
+
+public interface PersonListener {
+	public void dorun(Even even);
+
+	public void doeat(Even even);
+}
